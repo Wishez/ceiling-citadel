af85f5ddf10f9b9a07c1f7b8f94c70d5e72512cd
wizhes <shiningfinger@list.ru>
2018-01-25 13:48:56 +0300

    Add registered forms in admin panel.

--- ceiling/forms/admin.py
+++ ceiling/forms/admin.py
@@ -6,11 +6,52 @@ from .models import *
 
 @admin.register(Callback, site=admin_site)
 class CallbackAdmin(admin.ModelAdmin):
-    list_per_page = 10
-    list_display = ('status', 'created',)
+    list_per_page = 7
+    list_display = ('consumer', 'status', 'created',)
     ordering=('-created',)
     date_hierarchy = 'created'
-    filter_fields = ('status', 'created',)
-    search_fields = (
-        'status', 'created',
-    )
+    list_filter = ('consumer', 'status', 'created',)
+    search_fields = ('consumer', 'status', 'created',)
+
+@admin.register(Question, site=admin_site)
+class QuestionAdmin(admin.ModelAdmin):
+    list_per_page = 7
+    list_display = ('consumer', 'question', 'status', 'created',)
+    ordering = ('-created',)
+    date_hierarchy = 'created'
+    list_filter = ('consumer', 'status', 'created',)
+    search_fields = ('consumer', 'status', 'created',)
+
+
+
+@admin.register(Order, site=admin_site)
+class OrderAdmin(admin.ModelAdmin):
+    list_per_page = 7
+    list_display = ('consumer', 'status', 'order_id', 'order_price', 'created', 'modified',)
+    ordering = ('-created',)
+    date_hierarchy = 'created'
+    list_filter = ('consumer', 'status', 'created', 'order_id', 'order_price',)
+    search_fields = ('consumer', 'order_id', 'order_price', 'status', 'created',)
+    fieldsets = [
+        ('Данные заказа', {
+            'fields': (
+                ('consumer',),
+                ('ordered_products',),
+                ('price_will_be_counted', 'discount',),
+                ('order_price',),
+            )
+        }),
+        ('Статус и нормер заказа', {
+            'fields': (
+                ('order_id', 'status',),
+            )
+        }),
+        ('Локальные сообщения при смене статуса', {
+            'fields': (
+                ('after_success_closing_order_subject',),
+                ('after_success_closing_order_message',),
+                ('change_status_order_in_process_subject',),
+                ('change_status_order_in_process_message',),
+            )
+        }),
+    ]
