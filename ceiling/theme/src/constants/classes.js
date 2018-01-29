2b105577373b1a7c273f760088a568728cbe3595
wizhes <shiningfinger@list.ru>
2018-01-29 21:13:34 +0300

    Add written function that includes BEM enties if needed.

--- ceiling/theme/src/constants/classes.js
+++ ceiling/theme/src/constants/classes.js
@@ -1,22 +1,41 @@
 import _styles from '../index.sass'
 
+export const composeClasses = (block, element, modifier) => {
+	let composedClasses = {};
+
+	composedClasses.b = block;
+	if  (element)
+		composedClasses.el = element;
+	if (modifier)
+		composedClasses.m = modifier;
+
+	return composedClasses;		
+};
 
 function getClass({
-	b='', el='', m='', styles=_styles
+	b='', 
+	el='', 
+	m='', 
+	add='',
+	styles=_styles
 }) {
-	b = b ? styles[b] : b;
-	el = el ? styles[`${b}__${el}`] : el;
+	let additionalClasses = add
+		.split(' ')
+		.map(additionalClass => styles[additionalClass])
+		.join(' ');
+	
+	b = b ? styles[b] : '';
+	el = el ? styles[`${b}__${el}`] : '';
 	
 	if (el) 
 		b = '';
 
 	if (el && m) {
-		m =  m ? styles[`${b}__${el}_${m}`] : m;
+		m =  m ? styles[`${b}__${el}_${m}`] : '';
 	} else if (b && m) {
-		m =  m ? styles[`${b}_${m}`] : m;
+		m =  m ? styles[`${b}_${m}`] : '';
 	}
-
-	return `${b} ${el} ${m}`;
+	return `${b} ${el} ${m} ${additionalClasses}`;
 }
 
 export default getClass;
