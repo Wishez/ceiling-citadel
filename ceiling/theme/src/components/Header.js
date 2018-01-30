dec5b09b8cb7a3de8f4847cfeff7f573a682fbdb
wizhes <shiningfinger@list.ru>
2018-01-30 17:31:08 +0300

    Compose layout of header.

--- ceiling/theme/src/components/Header.js
+++ ceiling/theme/src/components/Header.js
@@ -3,19 +3,30 @@ import Logo from './Logo';
 import NavContainer from './../containers/NavContainer.js'; 
 import getClass from './../constants/classes';
 import Contacts from './Contacts';
-  
+import SearchContainer from './../containers/SearchContainer';
+import OrderButton from './OrderButton';
+import CallbackButton from './CallbackButton';
+import ButtonsGroup from './ButtonsGroup';
+
 const Header = ({
 	phone,
-	email
+	email,
+  quantityOrderedProducts
 }) => (
   	<header className={getClass({b: 'header'})}>
-      	<div className={getClass({b: 'container',  add: "parent row"})}>
+  	  <div className={getClass({b: 'container',  add: "parent row v-center"})}>
       		<NavContainer />
       		<Contacts 
-      			phone={`+7 (985) 905-12-51`}
-      			email={`shiningfinger@list.ru`}/>
-			<Logo />
-  		</div>
+      			phone={` +7 (985) 905-12-51`}
+      			email={` shiningfinger@list.ru`}/>
+			   <Logo />
+         <SearchContainer />
+         <ButtonsGroup className="baseChild">
+           <CallbackButton />
+           <OrderButton quantityOrderedProducts={0} />
+         </ButtonsGroup>
+
+      </div>
   	</header>
 );
         // <Contacts />
