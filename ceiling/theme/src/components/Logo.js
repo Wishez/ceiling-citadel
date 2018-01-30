857fafbbc19031c5074f95bb11ea04abc3def789
wizhes <shiningfinger@list.ru>
2018-01-30 17:29:52 +0300

    Change picture of logo and cover the text inside a simple container.

--- ceiling/theme/src/components/Logo.js
+++ ceiling/theme/src/components/Logo.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import getClass from './../constants/classes';
+import brand from './../images/logo.png';
+import Figure from './Figure';
 
 const Logo = ({}) => (
-  	<a href='/' className="brand">
-		<div className={getClass({b: "imageContainer", m: "brand"})}>
-			<figure className="imageContainer__image imageContainer__image_brand">
-			</figure>
-		</div>
-		<h1 className={getClass({b: 'brand', el: 'name'})}>
-			<span className={getClass({b: 'cian'})}>Art</span>
-			<span className={getClass({b: 'orange'})}>Ceil</span>
-		</h1>
-		<h2 className={`${getClass({b: "brand", el: "slogan"})} ${getClass({b: "darkBlue"})}`}>
-			Цитадель потолочных покрытий
-		</h2>
+  	<a href='/' className={getClass({b: "brand", add: "baseChild parent row h-between"})}>
+  		<Figure name="brand" 
+  			maxWidth={50}
+  			url={brand} 
+  			className="baseChild"
+  		/>
+  		<hgroup>
+			<h1 className={getClass({b: 'brand', el: 'name', add: "baseChild"})}>
+				<span className={getClass({b: 'cian'})}>Art</span>
+				<span className={getClass({b: 'orange'})}>Ceil</span>
+			</h1>
+			<h2 className={`${getClass({b: "brand", el: "slogan"})} ${getClass({b: "darkBlue"})}`}>
+				Цитадель потолочных покрытий
+			</h2>
+  		</hgroup>
 	</a>
 );
 
