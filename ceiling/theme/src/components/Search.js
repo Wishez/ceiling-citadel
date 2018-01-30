c9572555dffdd1155bda1364f4a04749367b8d74
wizhes <shiningfinger@list.ru>
2018-01-30 17:22:06 +0300

    Add search component.

---
+++ ceiling/theme/src/components/Search.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import getClass, { composeClasses } from './../constants/classes';
+import { Field, reduxForm } from 'redux-form';
+import Button from './Button';
+import RenderController from './RenderController';
+import colors from './../constants/colors';
+
+const Search = ({
+	block, 
+	element='',
+	modifier='',
+	href,
+	content,
+	children,
+	handleSubmit,
+	submitSearch,
+	...rest
+}) => (
+	<form className={getClass({
+			b: "searchForm",
+			add: "baseChild parent row h-between"
+
+		})}
+		onSubmit={handleSubmit(submitSearch.bind(this))}
+	>
+		<Field name="headerSearch"
+			type="text"
+			max-lenght="150"
+			placeholder="Поиск"
+			component={RenderController}
+		/>
+		<Button 
+			block="searchButton"
+			className="baseChild"
+			type="submit">
+			<svg id="search" width="45" height="45" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg"> 
+				<circle cx="75" cy="75" r="75" fill={colors.darkGray}></circle>
+				<circle cx="75" cy="75" r="55" fill={colors.orange}></circle>
+				<path d="M95 135 135 120 L180 180 150 200" ></path>
+			</svg>
+		</Button>
+	</form>
+);
+
+
+export default reduxForm({
+	form: "SearchForm"
+})(Search);
