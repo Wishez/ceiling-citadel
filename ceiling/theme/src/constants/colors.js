cdaa46b2c4658e1e797403db56ad8fadee4a0301
wizhes <shiningfinger@list.ru>
2018-01-30 17:19:45 +0300

    Add colors.

---
+++ ceiling/theme/src/constants/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+	darkGray: '#202020',
+	cian: '#4F7F76',
+	orange: '#C98C4D',
+	red: "#99303B",
+	gray: "#828282",
+	darkBlue: "#403866",
+	white: "#f7f7f7"
+};
+
+export default colors;
