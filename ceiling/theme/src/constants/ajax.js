fd8b2c3749acb42adf57c3274199bf2e4a472bc1
wizhes <shiningfinger@list.ru>
2018-02-01 14:51:30 +0300

    Add abstract function for making request with native javascript. And add conf file with helpfull variable.

--- ceiling/theme/src/constants/ajax.js
+++ ceiling/theme/src/constants/ajax.js
@@ -1,34 +1,31 @@
 import * as Cookies from 'js-cookie';
 
-const crossDomainRequest = (xhr, settings, that) => {		
-	/* Заметка:
-	 * csrftoken не всегда устанавливается сервером.
-	 * Оно закэшированно, если пользователь находится в своём аккаунте
-	 * Если даже csrftoken нет в кукАХ, то сервер в любом случае позволяет зарегистрироваться 
-	 * используя декоратор @csrf_exempt для функции регистрации.
-	 */
-	const csrftoken = Cookies.get('csrftoken');
-	const csrfSafeMethod = (method) => (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-	if (!csrfSafeMethod(settings.type) && !that.crossDomain) {
- 		xhr.setRequestHeader("X-CSRFToken", csrftoken);
-	}
-};
-
 const customAjaxRequest = ({
 	url,
 	data,
 	type,
+	crossDomain=false,
 	...rest
 }) => {
-	$.ajaxSetup({
-		url: url,
-		type: type,
-		data: data,
-		beforeSend(xhr, settings) {
-			crossDomainRequest(xhr, settings, this);
-		},
+	const headers = new Headers();
+
+	const csrftoken = Cookies.get('csrftoken');
+	const csrfSafeMethod = (method) => (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+
+	headers.append('Content-Type', 'application/json');
+
+	if (!csrfSafeMethod(type) && !crossDomain) {
+ 		headers.append("X-CSRFToken", csrftoken);
+	}
+	
+	const options= {
+		method: type,
+		headers,
+		body: JSON.stringify(data),
 		...rest
-	});
+	};
+
+	return new Request(url, options);
 
 };
 
