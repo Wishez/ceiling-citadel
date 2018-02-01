2dfe8f0dfb3c6f7fa9af78436995e621ecfe30bc
wizhes <shiningfinger@list.ru>
2018-02-01 11:43:26 +0300

    Add constants. Only cart's constants were added.

---
+++ ceiling/theme/src/constants/cart.js
@@ -0,0 +1,7 @@
+export const DELETE_PRODUCT = 'DELETE_PRODUCT';
+export const PUT_PRODUCT = 'PUT_PRODUCT';
+export const OPEN_CART = 'OPEN_CART';
+export const CLOSE_CART = 'CLOSE_CART';
+export const CHANGE_PRODUCT_QUANTITY = 'CHANGE_PRODUCT_QUANTITY';
+export const HIDE_HELP_TEXT = 'HIDE_HELP_TEXT';
+export const SHOW_HELP_TEXT = 'SHOW_HELP_TEXT';
