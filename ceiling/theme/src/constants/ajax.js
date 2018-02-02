09d84bc6335300bfe8309ebf5495c007882bc21b
wizhes <shiningfinger@list.ru>
2018-02-02 17:43:27 +0300

    Add new ajax abstract function for making requests.

--- ceiling/theme/src/constants/ajax.js
+++ ceiling/theme/src/constants/ajax.js
@@ -1,31 +1,61 @@
 import * as Cookies from 'js-cookie';
+import request from 'superagent';
+import nocache from 'superagent-no-cache';
 
 const customAjaxRequest = ({
 	url,
 	data,
-	type,
+	type='get',
 	crossDomain=false,
+	cache,
+	headers,
+	success,
+	failure,
 	...rest
 }) => {
-	const headers = new Headers();
+	let newRequest;
+
+	switch (type.toUpperCase()) {
+		case 'POST':
+			newRequest = request.post(url);
+			break;
+		case 'GET':
+			newRequest = request.get(url);
+			break;
+		default:
+			throw new Error(`You use "${type}" which is wrong request method!`)
+			break;
+	}
+
+	newRequest
+	  .send(data) // sends a JSON post body
+	  .set('accept', 'json');
 
 	const csrftoken = Cookies.get('csrftoken');
 	const csrfSafeMethod = (method) => (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 
-	headers.append('Content-Type', 'application/json');
+	if (headers && headers.length) {
+		headers.forEach(header => {
+			newRequest.set(header.name, header.value);
+		});
+	}
 
 	if (!csrfSafeMethod(type) && !crossDomain) {
- 		headers.append("X-CSRFToken", csrftoken);
+ 		newRequest.set("X-CSRFToken", csrftoken);
 	}
-	
-	const options= {
-		method: type,
-		headers,
-		body: JSON.stringify(data),
-		...rest
-	};
 
-	return new Request(url, options);
+	if (!cache) {
+		newRequest.user(nocache);
+	}
+
+	return newRequest.end((error, response) => {
+		if (/^(200|201|203)$/.test(response.statusCode.toString())) {
+			success(response);
+		} else {
+			failure(response.error);
+		}
+	});
+	
 
 };
 
