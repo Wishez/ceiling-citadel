36425adfd91ded8aabbee5d68a4aa5a9489a11b3
Filipp Zhuravlev <shiningfinger@list.ru>
2018-05-11 17:33:43 +0300

    Change the search's styles.

--- ceiling/theme/src/components/Search.js
+++ ceiling/theme/src/components/Search.js
@@ -28,6 +28,7 @@ const Search = ({
       className="baseChild"
       modifier="search"
       onChange={onChange}
+      showBorder={false}
       onFocus={(event) => {
         const element = event.target;
         element.focus();
@@ -41,7 +42,7 @@ const Search = ({
     <svg className={getClass({
       b: 'searchButton',
       add: 'baseChild parent row centered'
-    })} width="45" height="45" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg"> 
+    })} width="45" height="45" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
       <circle cx="75" cy="75" r="75" fill={colors.darkGray}></circle>
       <circle cx="75" cy="75" r="55" fill="#C98C4D"></circle>
       <path d="M95 135 135 120 L180 180 150 200" ></path>
