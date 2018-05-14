616ec5cffda7a6996bae79879e0746f568a41550
Filipp Zhuravlev <shiningfinger@list.ru>
2018-05-14 15:14:59 +0300

    Remove comments, replace section of div.

--- ceiling/theme/src/containers/Catalog/CatalogRoutes.js
+++ ceiling/theme/src/containers/Catalog/CatalogRoutes.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import {Route, Switch} from 'react-router-dom'; 
+import {Route, Switch} from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import getClass from './../../constants/classes';
@@ -15,12 +15,12 @@ import CategoryCollectionContainer from './CategoryCollectionContainer';
 import BrandProductContainer from './BrandProductContainer';
 import CategoryProductContainer from './CategoryProductContainer';
 
-import { 
+import {
   fetchCatalogEntityOrGetLocale
 } from './../../actions/catalog';
 import MyRoute from './../../components/MyRoute';
 
-import { selectNavigationItem } from './../../actions/navigationActions.js'; 
+import { selectNavigationItem } from './../../actions/navigationActions.js';
 import { initNavigationState } from './../../reducers/navigation.js';
 
 import {localData} from './../../constants/pureFunctions';
@@ -34,10 +34,10 @@ class CatalogRoutes extends Component {
   componentDidMount() {
     const { dispatch } = this.props;
 
-    
+
     // dispatch(tryFetchCatalog());
     dispatch(selectNavigationItem(initNavigationState.secondNavItem.index));
-    
+
     this.forceUpdate();
     if (!document.title)
       document.title = 'Каталог | ArtCeil';
@@ -45,27 +45,15 @@ class CatalogRoutes extends Component {
 
 
   render() {
-    const { 
+    const {
       url
     } = this.props.match;
     const {
       location
     } = this.props;
-  
+
     return (
-      <section className={getClass({b: 'catalog'})}>
-        {/*<TransitionGroup>
-          <CSSTransition key={location.key}  
-            classNames="translate"  
-            appear={true}
-            enter={true}          
-            exit={true}
-            timeout={{
-              appear: 800,
-              enter: 800,
-              exit: 300
-            }}
-          >*/}
+      <div className={getClass({b: 'catalog'})}>
         <Switch location={location}>
           <MyRoute path={`${url}/brand/:brandSlug/:collectionSlug/:productSlug`} component={BrandProductContainer} />
           <MyRoute path={`${url}/brand/:brandSlug/:collectionSlug`} component={BrandCollectionContainer} />
@@ -75,9 +63,7 @@ class CatalogRoutes extends Component {
           <MyRoute path={`${url}/category/:categorySlug`} component={CategoryContainer} />
           <MyRoute path={`${url}`} component={CatalogPageContainer} />
         </Switch>
-        {/*</CSSTransition>
-        </TransitionGroup>*/}
-      </section>
+      </div>
     );
   }
 }
