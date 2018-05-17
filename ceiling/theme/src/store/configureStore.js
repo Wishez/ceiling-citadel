7752c7d093647fdad3af78e0ea997fb492b32b59
Filipp Zhuravlev <shiningfinger@list.ru>
2018-05-17 17:40:50 +0300

    Make store for production.

--- ceiling/theme/src/store/configureStore.js
+++ ceiling/theme/src/store/configureStore.js
@@ -1,15 +1,22 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers/index';
 import { composeWithDevTools } from 'remote-redux-devtools';
+
+let compositionEnhance = null;
+if (process.env.NODE_ENV === 'development') {
+  compositionEnhance = composeWithDevTools(
+    applyMiddleware(thunkMiddleware)
+  );
+} else {
+  compositionEnhance = compose(applyMiddleware(thunkMiddleware));
+}
 
 export default function configureStore(initialState) {
   const store = createStore(
     rootReducer,
     initialState,
-    composeWithDevTools(
-      applyMiddleware(thunkMiddleware)
-    )
+    compositionEnhance
   );
 
   return store;
